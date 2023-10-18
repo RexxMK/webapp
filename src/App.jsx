6825a1a793cded17bc502931b3a13858d2c2b135
Svannad <112545494+Svannad@users.noreply.github.com>
2023-10-18 11:34:24 +0200

    stir

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from "react";
+import { Navigate, Route, Routes } from 'react-router-dom'
 import "./App.css";
+import Navigation from "./components/Navigation";
+import Hjem from "./pages/Hjem";
+import Lykken from "./pages/Lykken";
+import Find from "./pages/Find";
+import Tilfoj from "./pages/Tilfoj";
+import Favoritter from "./pages/Favoritter";
 
 function App() {
   return (
     <>
-      <p>HEJ RIKKE</p>
-      <p>HEJ DORTHE</p>
-      <p>HEJ SVANNA</p>
+      <Navigation/>
+      <Routes>
+        <Route path="/" element={<Hjem/>}/>
+        <Route path="/lykken" element={<Lykken/>}/>
+        <Route path="/find" element={<Find/>}/>
+        <Route path="/tilfoj" element={<Tilfoj/>}/>
+        <Route path="/favoritter" element={<Favoritter/>}/>
+        <Route path="*" element={<Navigate to="/"/>}/>
+      </Routes>
     </>
   );
 }
---
+++ src/pages/Find.jsx
@@ -0,0 +1,7 @@
+export default function Find () {
+    return (
+        <section>
+        <h1>Find Drink</h1>
+        </section>
+    )
+}
