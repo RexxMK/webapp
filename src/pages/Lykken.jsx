55623136e6c3b5c839e7889f57dc8c99e2b07e9a
eaadkkr <eaadkkr@students.eaaa.dk>
2023-10-19 20:14:05 +0200

    stir

--- src/pages/Lykken.jsx
+++ src/pages/Lykken.jsx
@@ -10,28 +10,32 @@ export default function Lykken() {
     // Variabel som kan være dark eller light.
     const [theme, setTheme] = useState("dark");
       
-    // Vi tjekker om temaet i body er dark eller light og sætter currentTheme lig den pågældende.
+    // Vi tjekker om temaet i body er dark eller light og sætter currentTheme lig denne.
     useEffect(() => {
         const currentTheme = document.querySelector("body").getAttribute('data-theme');
 
-          if (currentTheme === "dark" || currentTheme === "light") {
-            setTheme(currentTheme);
+          // Hvis temaet i body er light sættes theme til light. Ellers dark.
+          if (currentTheme === "light") {
+            setTheme("light");
+          } else {
+            setTheme("dark");
           }
 
-          console.log("Current Theme:", currentTheme);
+          console.log("Current Theme:", theme);
+
+        // useEffect skal køre én gang når siden loades og så hver gang der er en ændring i theme.
         }, [theme]);
 
 
         return (
-            
             <section className="fixedMargin">
 
               <h1>Prøv Lykken!</h1>
 
-              {/* Display the image based on the current theme */}
-              <img src={theme === "dark" ? darkShaker : lightShaker} alt="Tegnet GIF af en drinkshaker, der shaker" className="lykkenImg"/>
+              {/* GIF'en der vises afhænger af theme. Hvis theme er light vises lightShaker, ellers darkShaker */}
+              <img src={theme === "light" ? lightShaker : darkShaker} alt="Tegnet GIF af en drinkshaker, der shaker" className="lykkenImg"/>
 
-              <h3>Shake din mobil!</h3>
+              <h3 className="shakeTxt">Shake din mobil!</h3>
 
               <Knap className="buttonFull" label="Eller klik her" />
            
