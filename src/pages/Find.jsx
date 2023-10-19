b62b181779c3242eea19d5e1e2be964b4eb35188
Svannad <112545494+Svannad@users.noreply.github.com>
2023-10-19 12:01:02 +0200

    stir

--- src/pages/Find.jsx
+++ src/pages/Find.jsx
@@ -1,7 +1,11 @@
+import DrinkCard from "../components/DrinkCard";
+import Dummy from '../img/tomside-light.png'
+
 export default function Find () {
     return (
         <section>
         <h1>Find Drink</h1>
+        <DrinkCard img={Dummy} titel="Dummy Drink"/>
         </section>
     )
 }
