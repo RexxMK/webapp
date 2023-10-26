7472e9616ed44b888db93e4538e83de72bc35b64
eaadkkr <eaadkkr@students.eaaa.dk>
2023-10-26 13:06:42 +0200

    stir

--- src/pages/LykkenDetaljeside.jsx
+++ src/pages/LykkenDetaljeside.jsx
@@ -9,9 +9,7 @@ import Drink from "../components/Drink";
 export default function LykkenDetaljeside() {
 
     const params = useParams();
-
     const [currentDrink, setCurrentDrink] = useState({});
-
     const url =
           `https://webapp-68213-default-rtdb.europe-west1.firebasedatabase.app/drinks/${params.currentDrinkId}.json`;
   
@@ -23,17 +21,60 @@ export default function LykkenDetaljeside() {
       }
       getCurrentDrink();
     }, [url]);
+
+
+
+
+    // Deler ingredienserne i en liste
+    const ingredienserListe = currentDrink.ingredienser.map((ingrediens, index) => (
+        <li key={index}>{ingrediens}</li>
+    ));
+
+    // Deler metoden i separate <p> elementer
+    const metodeAfsnit = currentDrink.metode.map((afsnit, index) => (
+        <li key={index}>{afsnit}</li>
+    ));
+
+    const history = useNavigate();
     
 
   
   
   
     return (
-      <article>
-   
-            <Drink key={currentDrink.id} drink={currentDrink} />
+            
+        <section className="op-wrap">
+            
+            {/*Vi laver en onclick function
+            Så ved klik af knappen så går du et tilbage (-1) af din webbrowser historik*/}
+            <button className="back" onClick={() => history(-1)}>
+                <ArrowBackRoundedIcon />
+            </button>
 
-      </article>
+            <img className="opimg" src={currentDrink.billede} alt="Billede af drink" />
+            
+            <div className="fixedMargin">
+                <div className="opheader">
+                    <h2>{currentDrink.navn}</h2>
+                    <div className="op-like">
+                        <FavoritHjerteDrink />
+                    </div>
+                </div>
+                <h3>Ingredienser</h3>
+                <div className="items">
+          <ul className="items">{ingredienserListe}</ul>
+        </div>
+        <h3>Fremgangsmåde</h3>
+        <div className="howto">
+          <ul>{metodeAfsnit}</ul>
+        </div>
+        <h3>Bedømmelse</h3>
+        <StarRating />
+      </div>
+    </section>
+            
     );
   }
+
+  // <Drink key={currentDrink.id} drink={currentDrink} />
   
