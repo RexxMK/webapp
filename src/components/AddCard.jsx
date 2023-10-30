d28faffd6f855c0db54d205718462e4adca34995
RexxMK <rikkekodahl@live.dk>
2023-10-30 13:14:38 +0100

    tilføj fikset

--- src/components/AddCard.jsx
+++ src/components/AddCard.jsx
@@ -3,25 +3,24 @@ import { useNavigate } from "react-router-dom";
 
 //SD
 
-export default function AddCard({tilfoj}) {
-    const navigate = useNavigate();
+export default function AddCard({ tilfoj }) {
+  const navigate = useNavigate();
 
   function handleClick() {
+    navigate(`tilfojs/${tilfoj.id}`);
+  }
 
-    navigate(`tilfojs/${tilfoj.id}`); 
-  }
-    
-    return (
-      <div className="drinkcard" onClick={handleClick}>
-        <img src={tilfoj.billede} alt={tilfoj.navn} className="drinkimg" />
-        <h3 className="drinkheader">{tilfoj.navn}</h3>
-        <div className="star-wrap">
-          <StarBorderRoundedIcon />
-          <StarBorderRoundedIcon />
-          <StarBorderRoundedIcon /> 
-          <StarBorderRoundedIcon />
-          <StarBorderRoundedIcon />
-        </div>
+  return (
+    <div className="drinkcard" onClick={handleClick}>
+      <img src={tilfoj.billede} alt={tilfoj.navn} className="drinkimg" />
+      <h3 className="drinkheader">{tilfoj.navn}</h3>
+      <div className="star-wrap">
+        <StarBorderRoundedIcon />
+        <StarBorderRoundedIcon />
+        <StarBorderRoundedIcon />
+        <StarBorderRoundedIcon />
+        <StarBorderRoundedIcon />
       </div>
-    );
-  }
+    </div>
+  );
+}
