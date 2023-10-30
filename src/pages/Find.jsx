4739b2dd94920089462da93aa3fefee7a94e759c
eaadkkr <eaadkkr@students.eaaa.dk>
2023-10-30 13:14:18 +0100

    stir

--- src/pages/Find.jsx
+++ src/pages/Find.jsx
@@ -8,89 +8,25 @@ import SearchIcon from '@mui/icons-material/Search';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import RestartAltIcon from '@mui/icons-material/RestartAlt';
 
-//RMK
-/*export default function Find() {
+
+// DKK
+
+
+export default function Find() {
+
+  // RMK
   // Her opretter jeg to tilstandsvariabler ved hjælp af "useState".
   //"drinks" bruges til at lagre listen over drinks, og "isDrinks" bruges til at kontrollere, om der er drinks at vise.
   const [drinks, setDrinks] = useState([]);
   const [isDrinks, setIsDrinks] = useState(true);
 
-  useEffect(() => {
-    async function getDrinks() {
-      //Der defineres en URL til at hente drinks-data fra vores Firebase-database.
-      const url =
-        "https://webapp-68213-default-rtdb.europe-west1.firebasedatabase.app/drinks.json";
-
-      //Her bruges "fetch" til hente drinks-data fra vores Firebase-database og konvertere dem til JSON-format.
-      const response = await fetch(url);
-      const data = await response.json();
-
-      //Hvis der er data tilgængelig, laves dataerne til et array og opdaterer "drinks" til at indeholde denne liste af drinks.
-      if (data !== null) {
-        const drinksArray = Object.keys(data).map((key) => ({
-          id: key,
-          ...data[key],
-        }));
-        setDrinks(drinksArray);
-      }
-
-      //Hvis der ikke er nogen data tilgængelig, opdateres "isDrinks" til "false" for at vise en meddelelse om, at der ikke er noget at vise.
-      else {
-        setIsDrinks(false);
-      }
-    }
-    getDrinks();
-  }, []);
-
-  //Kopieret kode fra DK LightMode component
-    //at få billedet at skiftes imellem light og dark mode
-    const [theme, setTheme] = useState("dark");
-      
-    useEffect(() => {
-        const currentTheme = document.querySelector("body").getAttribute('data-theme');
-
-    if (currentTheme === "light") {
-      setTheme("light");
-    } else {
-      setTheme("dark");
-    }
-
-        }, []);
-
-
-  //Hvis "isDrinks" er "true", vises en liste af drinks vha. "map" funktionen, ellers vises en besked om, at der ikke er noget at vise.
-  return (
-    <article className="page">
-
-
-      {isDrinks ? (
-        <div className="flexbox">
-          {drinks.map((drink) => (
-            <DrinkKort key={drinkOne.id} drink={drinkOne} />
-          ))}
-        </div>
-      ) : (
-        //SD
-        <div className="fixedMargin tomside"> 
-          <p>Din søgning gav 0 resultater.</p>
-          <img src={theme === "light" ? tomsideLight : tomsideDark} id="tomsidebillede"/>
-          <h4>Du vil måske synes om</h4>
-        </div>
-      )}
-    </article>
-  );
-}*/
-
-//RMK
-export default function Find() {
-  // Her opretter jeg to tilstandsvariabler ved hjælp af "useState".
-  //"drinks" bruges til at lagre listen over drinks, og "isDrinks" bruges til at kontrollere, om der er drinks at vise.
-  const [drinks, setDrinks] = useState([]);
-  const [isDrinks, setIsDrinks] = useState(false);
+  // DKK
+  // Til at lave søgefunktionen oprettes yderligere to variable.
   const [soegeTekst, setSoegeTekst] = useState("");
   const [skyggeDrinksListe, setSkyggeDrinksListe] = useState([]);
   
 
+  // RMK
   useEffect(() => {
     async function getDrinks() {
       //Der defineres en URL til at hente drinks-data fra vores Firebase-database.
@@ -117,45 +53,59 @@ export default function Find() {
       }
     }
     getDrinks();
-    const currentTheme = document
-      .querySelector("body")
-      .getAttribute("data-theme");
 
-    if (currentTheme === "light") {
-      setTheme("light");
-    } else {
-      setTheme("dark");
-    }
   }, []);
 
+
+  // DKK
+  // Når brugeren resetter deres søgning, skal der ske 2 ting.
+  // 1. Alle drinks skal igen vises. Derfor tages sættes skyggeDrinksListe til drinks (som netop er alle drinks).
+  // 2. Brugerens søgetekst skal slettes fra søgefeltet. Den sættes derfor til en tom streng.
   function reset() {
     setSkyggeDrinksListe(drinks);
     setSoegeTekst("");
-    setIsDrinks(false);
   }
 
+  // DKK
+  // handleSubmit-funktionen skal kaldes, når brugeren laver en søgning.
   function handleSubmit(e) {
+
+    // Normalt genindlæses siden ved en formularindsendelse. Dette forhindres her med preventDefault.
     e.preventDefault();
+
+    /* Med filter-metoden gennemgås hvert element i drinks og det vurderes om elementet skal inkluderes i det nye array, soegeResultat, 
+    baseret på de to betingelser, s1 og s2. */ 
     let soegeResultat = drinks.filter((drink) => {
-      const s1 = drink.navn.toLowerCase().includes(soegeTekst.toLowerCase()); // Søg i drink navn
+
+      // Med s1 tjekkes om søgeteksten (som er gemt i soegeTekst) er inkluderet i navnet på en drink.
+      // toLowerCase() sørger for at alt læses med små bogstaver, så der ikke gøres forskel på små og store bogstaver.
+      const s1 = drink.navn.toLowerCase().includes(soegeTekst.toLowerCase());
+
+      // Med s2 tjekkes om søgeteksten er inkluderet i ingredienserne til en drink.
       const s2 = drink.ingredienser.find((ingrediens) =>
         ingrediens.toLowerCase().includes(soegeTekst.toLowerCase())
-      ); // Søg blandt ingredienser
-      return s1 || s2; // Hvis enten s1 eller s2 er sand, så eksisterer søgeteksten i drink navn eller ingredienser
+      );
+
+      // Hvis enten s1 eller s2 er sand, eksisterer søgeteksten i drinks navn eller ingredienser.
+      return s1 || s2;
     });
 
+    // Hvis længden på søgeresultatet er 0, betyder det, at der ikke blev fundet nogen drinks, der matcher søgeteksten.
     if (soegeResultat.length === 0) {
-      // Er der ingen drinks som matcher søgetekst, så er der ingen drinks at vise
+
+      // I så fald er der ingen drinks at vise.
       setIsDrinks(false);
     } else {
+
+      // Ellers er der drinks at vise, hvilket sker ved at sætte skyggeDrinksListe til at være søgeresultatet.
       setIsDrinks(true);
-      setSkyggeDrinksListe(soegeResultat); // Sæt skyggelisten lig med resultat af søgning
+      setSkyggeDrinksListe(soegeResultat);
     }
   }
 
 
-  //Kopieret kode fra DK LightMode component
-    //at få billedet at skiftes imellem light og dark mode
+    // Kopieret kode fra DKK Lykken page
+    // Til at få billedet at skiftes imellem light og dark mode
     const [theme, setTheme] = useState("dark");
       
     useEffect(() => {
@@ -170,7 +120,7 @@ export default function Find() {
     }, []);
 
 
-
+  // DKK
   return (
     <article className="page">
 
@@ -179,6 +129,7 @@ export default function Find() {
         <button className="filterButton buttonFull"><FilterAltIcon className="filterIcon"/> Filtrer </button>
       </div>
 
+      
      
       <form onSubmit={handleSubmit} className="fixedMargin">
         <div className="searchFormDiv">
@@ -201,7 +152,7 @@ export default function Find() {
           </button>
         </div>
       </form>
-      
+
 
       {isDrinks ? (
         <div className="flexbox">
@@ -209,8 +160,9 @@ export default function Find() {
             <DrinkKort key={drink.id} drink={drink} />
           ))}
         </div>
+
       ) : (
-        //SD
+
         <div className="fixedMargin tomside">
           <p>Din søgning gav 0 resultater</p>
           <img src={theme === "light" ? tomsideLight : tomsideDark} id="tomsidebillede" />
