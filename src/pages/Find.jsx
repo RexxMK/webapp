fda2d727757c7967007f3f2918edd7ad0aafdf60
eaadkkr <eaadkkr@students.eaaa.dk>
2023-10-30 12:02:43 +0100

    stir

--- src/pages/Find.jsx
+++ src/pages/Find.jsx
@@ -4,13 +4,14 @@ import tomsideLight from "../img/tomside-light.png";
 import tomsideDark from "../img/tomside-dark.png";
 import Knap from "../components/Knap";
 import Drink from "../components/Drink";
+import SearchIcon from '@mui/icons-material/Search';
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
 
 
 //RMK
 /*export default function Find() {
 
-
-export default function Find() {
 
 
   // Her opretter jeg to tilstandsvariabler ved hjælp af "useState".
@@ -66,17 +67,21 @@ export default function Find() {
   );
 }*/
 
+
+
 //RMK
+
+
 export default function Find() {
+
   // Her opretter jeg to tilstandsvariabler ved hjælp af "useState".
   //"drinks" bruges til at lagre listen over drinks, og "isDrinks" bruges til at kontrollere, om der er drinks at vise.
   const [drinks, setDrinks] = useState([]);
   const [isDrinks, setIsDrinks] = useState(false);
+  
   const [soegeTekst, setSoegeTekst] = useState("");
   const [skyggeDrinksListe, setSkyggeDrinksListe] = useState([]);
-  //Kopieret kode fra DK LightMode component
-  //at få billedet at skiftes imellem light og dark mode
-  const [theme, setTheme] = useState("dark");
+  
 
   useEffect(() => {
     async function getDrinks() {
@@ -104,15 +109,7 @@ export default function Find() {
       }
     }
     getDrinks();
-    const currentTheme = document
-      .querySelector("body")
-      .getAttribute("data-theme");
-
-    if (currentTheme === "light") {
-      setTheme("light");
-    } else {
-      setTheme("dark");
-    }
+    
   }, []);
 
   function reset() {
@@ -140,178 +137,52 @@ export default function Find() {
     }
   }
 
-  //Hvis "isDrinks" er "true", vises en liste af drinks vha. "map" funktionen, ellers vises en besked om, at der ikke er noget at vise.
-  return (
-    <article className="page">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="search"
-          style={{ height: "25px", marginRight: "20px" }}
-          required
-          value={soegeTekst}
-          onChange={(e) => setSoegeTekst(e.target.value)}
-        />
-        <button
-          type="submit"
-          style={{ backgroundColor: "gray", marginRight: "15px" }}
-        >
-          Søg
-        </button>
-        <button type="button" onClick={reset}>
-          Reset
-        </button>
-      </form>
-      {isDrinks ? (
-        <div
-          style={{
-            display: "flex",
-            flexWrap: "wrap",
-            gap: "10px",
-            marginTop: "10px",
-          }}
-        >
-          {skyggeDrinksListe.map((drink) => (
-            <DrinkKort key={drink.id} drink={drink} />
-          ))}
-        </div>
-      ) : (
-        <div className="fixedMargin tomside">
-          <p style={{ marginTop: "20px" }}>Tilføj ny drink.</p>
-          <img
-            src={theme === "light" ? tomsideLight : tomsideDark}
-            id="tomsidebillede"
-          />
-          <Knap
-            to={"/tilfoj"}
-            className={"buttonFull"}
-            label={"Tilføj ny drink!"}
-          />
-        </div>
-      )}
-    </article>
-  );
-}*/
 
-//RMK
-export default function Find() {
-  // Her opretter jeg to tilstandsvariabler ved hjælp af "useState".
-  //"drinks" bruges til at lagre listen over drinks, og "isDrinks" bruges til at kontrollere, om der er drinks at vise.
-  const [drinks, setDrinks] = useState([]);
-  const [isDrinks, setIsDrinks] = useState(false);
-  const [soegeTekst, setSoegeTekst] = useState("");
-  const [skyggeDrinksListe, setSkyggeDrinksListe] = useState([]);
-  //Kopieret kode fra DK LightMode component
-  //at få billedet at skiftes imellem light og dark mode
+  // Kopieret kode fra DKK Lykken page
+  // Til at få billedet at skiftes imellem light og dark mode
   const [theme, setTheme] = useState("dark");
-
+      
   useEffect(() => {
-    async function getDrinks() {
-      //Der defineres en URL til at hente drinks-data fra vores Firebase-database.
-      const url =
-        "https://webapp-68213-default-rtdb.europe-west1.firebasedatabase.app/drinks.json";
-
-      //Her bruges "fetch" til hente drinks-data fra vores Firebase-database og konvertere dem til JSON-format.
-      const response = await fetch(url);
-      const data = await response.json();
-
-      //Hvis der er data tilgængelig, laves dataerne til et array og opdaterer "drinks" til at indeholde denne liste af drinks.
-      if (data !== null) {
-        const drinksArray = Object.keys(data).map((key) => ({
-          id: key,
-          ...data[key],
-        }));
-        setDrinks(drinksArray);
-        setSkyggeDrinksListe(drinksArray);
-      }
-
-      //Hvis der ikke er nogen data tilgængelig, opdateres "isDrinks" til "false" for at vise en meddelelse om, at der ikke er noget at vise.
-      else {
-        setIsDrinks(false);
-      }
-    }
-    getDrinks();
-    const currentTheme = document
-      .querySelector("body")
-      .getAttribute("data-theme");
+    const currentTheme = document.querySelector("body").getAttribute('data-theme');
 
     if (currentTheme === "light") {
       setTheme("light");
     } else {
       setTheme("dark");
     }
+
   }, []);
 
-  function reset() {
-    setSkyggeDrinksListe(drinks);
-    setSoegeTekst("");
-    setIsDrinks(false);
-  }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    let soegeResultat = drinks.filter((drink) => {
-      const s1 = drink.navn.toLowerCase().includes(soegeTekst.toLowerCase()); // Søg i drink navn
-      const s2 = drink.ingredienser.find((ingrediens) =>
-        ingrediens.toLowerCase().includes(soegeTekst.toLowerCase())
-      ); // Søg blandt ingredienser
-      return s1 || s2; // Hvis enten s1 eller s2 er sand, så eksisterer søgeteksten i drink navn eller ingredienser
-    });
 
-    if (soegeResultat.length === 0) {
-      // Er der ingen drinks som matcher søgetekst, så er der ingen drinks at vise
-      setIsDrinks(false);
-    } else {
-      setIsDrinks(true);
-      setSkyggeDrinksListe(soegeResultat); // Sæt skyggelisten lig med resultat af søgning
-    }
-  }
 
-  //Hvis "isDrinks" er "true", vises en liste af drinks vha. "map" funktionen, ellers vises en besked om, at der ikke er noget at vise.
   return (
     <article className="page">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="search"
-          style={{ height: "25px", marginRight: "20px" }}
-          required
-          value={soegeTekst}
-          onChange={(e) => setSoegeTekst(e.target.value)}
-        />
-        <button
-          type="submit"
-          style={{ backgroundColor: "gray", marginRight: "15px" }}
-        >
-          Søg
-        </button>
-        <button type="button" onClick={reset}>
-          Reset
-        </button>
+
+      <div className="fixedMargin filterHeaderDiv">
+        <h1>Find Drink</h1>
+        <button className="filterButton buttonFull"><FilterAltIcon className="filterIcon"/>Filtrer</button>
+      </div>
+
+      <form onSubmit={handleSubmit} className="fixedMargin">
+        <div className="searchFormDiv">
+          <input type="search" required value={soegeTekst} placeholder="Søg efter en drink" onChange={(e) => setSoegeTekst(e.target.value)} className="search"/>
+          <button type="button" onClick={reset} className="resetButton"><RestartAltIcon /></button>
+          <button type="submit" className="searchButton"><SearchIcon /></button>
+        </div>
       </form>
+
       {isDrinks ? (
-        <div
-          style={{
-            display: "flex",
-            flexWrap: "wrap",
-            gap: "10px",
-            marginTop: "10px",
-          }}
-        >
+        <div className="flexbox">
           {skyggeDrinksListe.map((drink) => (
             <DrinkKort key={drink.id} drink={drink} />
           ))}
         </div>
       ) : (
-        <div className="fixedMargin tomside">
-          <p style={{ marginTop: "20px" }}>Tilføj ny drink.</p>
-          <img
-            src={theme === "light" ? tomsideLight : tomsideDark}
-            id="tomsidebillede"
-          />
-          <Knap
-            to={"/tilfoj"}
-            className={"buttonFull"}
-            label={"Tilføj ny drink!"}
-          />
+        <div className="flexbox">
+          {drinks.map((drink) => (
+            <DrinkKort key={drink.id} drink={drink} />
+          ))}
         </div>
       )}
     </article>
